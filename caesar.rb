a377138439cc7ac72e7b7913a5f182dd10f6b3bf
Aaron <aaronmcdonald3d@gmail.com>
2020-05-25 15:44:56 -0400

    first solution to caesar_cipher

--- caesar.rb
+++ caesar.rb
@@ -1,1 +1,30 @@
-puts "Hello world"
+def caesar_cipher(string_in, shift)
+    while(shift > 26)
+        shift -= 26
+    end
+    while(shift < 0)
+        shift += 26
+    end
+    string_array = string_in.split('')
+    string_out = string_array.map do |c|
+        code = c.ord
+        if(code >= 97 && code <= 122)
+            code += shift
+            if(code >= 97 && code <= 122)
+                c = code.chr
+            else
+                c = (code - 26).chr
+            end
+        elsif(code >= 65 && code <= 90)
+            code += shift
+            if(code >= 65 && code <= 90)
+                c = code.chr
+            else
+                c = (code - 26).chr
+            end
+        else
+            c = c
+        end
+    end
+    return string_out.join('')
+end
